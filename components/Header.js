4df37ea798e1aa6b6aa35cdfc3f4953e5704c881
Lucas Soares de Brito <ext-lucas.brito@bbts.com.br>
2021-07-16 23:57:25 -0300

    Tailwind CSS configurado e pronto para uso com Next.js

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import { SearchIcon } from "@heroicons/react/outline"
+
+const Header = () => {
+    return (
+        <div>
+            <SearchIcon className="h-6"/>
+        </div>
+    )
+}
+
+export default Header
